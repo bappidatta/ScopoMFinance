1ec004ab8d9c0ab6e04cb54149a78a2814c9c90c
Hasib Tarafder <arnab.sust@gmail.com>
2017-02-02 18:22:43 +0600

    Added OrgCreditOfficer table

---
+++ ScopoFinance.Database/Tables/OrgCreditOfficer.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[OrgCreditOfficer]
+(
+	[Id] INT NOT NULL IDENTITY,
+	[BranchId] INT NOT NULL,
+	[EmployeeId] INT NOT NULL,
+	[OrganizationId] INT NOT NULL,
+	[AssignedDate] DATETIME NOT NULL,
+	[ReleaseDate] DATETIME NULL,
+	[IsActive] BIT NOT NULL DEFAULT 1, 
+    [IsDeleted] BIT NOT NULL DEFAULT 0, 
+    [CreatedBy] NVARCHAR(256) NOT NULL, 
+    [CreatedOn] DATETIME NOT NULL,
+	[UpdatedBy] NVARCHAR(256) NULL, 
+    [UpdatedOn] DATETIME NULL,
+	CONSTRAINT [PK_OrgCreditOfficer] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_OrgCO_Branch] FOREIGN KEY ([BranchId]) REFERENCES [Branch]([Id]),
+	CONSTRAINT [FK_OrgCO_Employee] FOREIGN KEY ([EmployeeId]) REFERENCES [Employee]([Id]),
+	CONSTRAINT [FK_OrgCO_Org] FOREIGN KEY ([OrganizationId]) REFERENCES [Organization]([Id])
+)
