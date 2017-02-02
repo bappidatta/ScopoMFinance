5dfccc9c1ba5e02feb52a90e75177058da70915e
Hasib Tarafder <arnab.sust@gmail.com>
2017-02-02 16:45:25 +0600

    Added BranchWiseProjectMapping table

---
+++ ScopoFinance.Database/Tables/BranchWiseProjectMapping.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[BranchWiseProjectMapping]
+(
+	[Id] INT NOT NULL IDENTITY, 
+	[BranchId] INT NOT NULL,
+	[ProjectId] INT NOT NULL,
+	CONSTRAINT [PK_BranchWiseProjectMapping] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_BranchProject_Project] FOREIGN KEY ([ProjectId]) REFERENCES [Project]([Id]),
+	CONSTRAINT [FK_BranchProject_Branch] FOREIGN KEY ([BranchId]) REFERENCES [Branch]([Id])
+)
