22720b8bc0fc795bea7ff86ebbef4a96ec972932
Bappi Datta <bappigeek@gmail.com>
2017-02-20 01:42:29 +0600

    Add dashboard widget & modify ProjectType Service

--- ScopoMFinance.Core/Services/ProjectTypeService.cs
+++ ScopoMFinance.Core/Services/ProjectTypeService.cs
@@ -1,5 +1,8 @@
-﻿using ScopoMFinance.Core.Helpers;
+﻿using NtitasCommon.Core.Common;
+using ScopoMFinance.Core.Helpers;
+using ScopoMFinance.Domain.Models;
 using ScopoMFinance.Domain.Repositories;
+using ScopoMFinance.Domain.ViewModels.Policy;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +14,10 @@ namespace ScopoMFinance.Core.Services
     public interface IProjectTypeService
     {
         List<DropDownHelper> GetProjectTypeDropDown();
+        ProjectTypeSetupViewModel GetProjectTypeById(int projectTypeId);
+        PList<ProjectTypeListViewModel> GetProjectTypeList(int pageNumber, int pageSize, SortDirection sortDir, int sortCol);
+        void CreateProjectType(ProjectTypeSetupViewModel vm);
+        void UpdateProjectType(ProjectTypeSetupViewModel vm);
     }
 
     public class ProjectTypeService : IProjectTypeService
@@ -25,13 +32,39 @@ namespace ScopoMFinance.Core.Services
         public List<DropDownHelper> GetProjectTypeDropDown()
         {
             var projectTypeDropDown = from c in _uow.ProjectTypeRepository.Get(x => x.IsActive == true && x.IsDeleted == false)
-                                       select new DropDownHelper()
-                                       {
-                                           Value = c.Id,
-                                           Text = c.Name
-                                       };
+                                      select new DropDownHelper()
+                                      {
+                                          Value = c.Id,
+                                          Text = c.Name
+                                      };
 
             return projectTypeDropDown.ToList();
+        }
+
+        public ProjectTypeSetupViewModel GetProjectTypeById(int projectTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PList<ProjectTypeListViewModel> GetProjectTypeList(int pageNumber, int pageSize, SortDirection sortDir, int sortCol)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void CreateProjectType(ProjectTypeSetupViewModel vm)
+        {
+            ProjectType projectType = new ProjectType
+            {
+
+            };
+
+            _uow.ProjectTypeRepository.Insert(projectType);
+            _uow.Save();
+        }
+
+        public void UpdateProjectType(ProjectTypeSetupViewModel vm)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ ScopoMFinance.Domain/ViewModels/Policy/ProjectTypeListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScopoMFinance.Domain.ViewModels.Policy
+{
+    public class ProjectTypeListViewModel
+    {
+
+    }
+}
