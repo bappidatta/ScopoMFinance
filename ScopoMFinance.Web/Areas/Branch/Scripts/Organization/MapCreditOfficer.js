0d2c49a6b240a4971856fea0146b6bb30adaf57e
Hasib Tarafder <arnab.sust@gmail.com>
2017-02-26 10:47:07 +0600

    Change javascript of MapCreditOfficer

--- ScopoMFinance.Web/Areas/Branch/Scripts/Organization/MapCreditOfficer.js
+++ ScopoMFinance.Web/Areas/Branch/Scripts/Organization/MapCreditOfficer.js
@@ -1,7 +1,10 @@
 ﻿$(function () {
     $('#CreditOfficerId').on('change', function () {
         var $this = $(this);
-        $('#mappedListContainer').load('/Branch/Organization/CreditOfficerMappedList/' + $this.val());
+        if ($this.val())
+            $('#mappedListContainer').load('/Branch/Organization/CreditOfficerMappedList/' + $this.val());
+        else
+            $('#mappedListContainer').html('');
     });
 
     if ($('#CreditOfficerId').val())
